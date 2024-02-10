2b0801eb94d36d3646eaadeec5ee0c04c47dc429
Shivendra Bind <shivendra.bind@gmail.com>
2024-02-10 21:59:19 +0530

    1.2 Hello, YOU

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,9 +3,9 @@ package hello
 import "testing"
 
 func TestHello(t *testing.T) {
-	want := "Hello, World"
+	want := "Hello, Chrish"
 
-	got := Hello()
+	got := Hello("Chrish")
 
 	if want != want {
 		t.Errorf("want %q, got %q ", want, got)
---
+++ notes/testing.md
@@ -0,0 +1,6 @@
+# Testing in go
+
+### rules
+1. File name format should be like `filename_test.go`
+2. Test function must start with with word `Test`
+3. Test function takes arg `t *testing.T`
