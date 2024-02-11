6d1a72c77cb2764fff36994d1b1d93102c199e13
Shivendra Bind <shivendra.bind@gmail.com>
2024-02-11 18:41:05 +0530

    4.4-code

--- arrays/sum.go
+++ arrays/sum.go
@@ -28,3 +28,16 @@ func SumAllAppend(numbersToSum ...[]int) []int {
 
 	return sums
 }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			continue
+		}
+		sums = append(sums, Sum(numbers[1:]))
+	}
+
+	return sums
+}
