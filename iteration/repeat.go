dc8bcad07750070a6df1cae7602f093298ced9f6
Shivendra Bind <shivendra.bind@gmail.com>
2024-02-11 14:55:39 +0530

    3.2 repeat charecter by given no.

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(char string) string {
+func Repeat(char string, iteration int) string {
 	var result string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < iteration; i++ {
 		result = result + char
 	}
 	return result
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -4,9 +4,20 @@ import "testing"
 
 func TestRepeat(t *testing.T) {
 	t.Run("repeat charecter 5 times", func(t *testing.T) {
-		got := Repeat("q")
+		got := Repeat("q", 5)
 
 		expected := "qqqqq"
+		if got != expected {
+			t.Errorf("expected %q, got %q", expected, got)
+		}
+	})
+
+	t.Run("repeat charecter given no. of times", func(t *testing.T) {
+		iteration := 10
+
+		got := Repeat("r", iteration)
+		expected := "rrrrrrrrrr"
+
 		if got != expected {
 			t.Errorf("expected %q, got %q", expected, got)
 		}
@@ -16,7 +27,7 @@ func TestRepeat(t *testing.T) {
 func BenchmarkRepeat(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 10)
 	}
 
 }
