914187970af64457875262b22e48310f4d4c07ea
shivendra-bind <shivendra.bind@gmail.com>
2024-05-14 22:25:27 +0530

    dictionary: test search

---
+++ maps/dictionary_test.go
@@ -0,0 +1,18 @@
+package maps
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := map[string]string{"test": "This is just a test"}
+	got := Search(dictionary, "test")
+	want := "This is just a test"
+	assertString(t, got, want)
+}
+
+func assertString(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q , given %q", got, want, "test")
+	}
+}
